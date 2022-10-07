67e1daff2674617e6c3345d1a82acfef5cf70d59
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-10-07 01:28:40 -0700

    Finishing for today

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,15 +10,15 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['id', 'product_name',  'price', 'category_id'],
+        attributes: ['id', 'product_name', 'price', 'category_id'],
       }
     ]
   })
-  .then(tagData => res.json(tagData))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
@@ -37,8 +37,8 @@ router.get('/:id', (req, res) => {
   })
     .then(tagData => {
       // if no data tag
-      if(!tagData) {
-        res.status(404).json({ message: 'No id found given the tag'});
+      if (!tagData) {
+        res.status(404).json({ message: 'No id found given the tag' });
         return;
       }
       res.json(tagData);
@@ -52,14 +52,55 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tagName: req.body.tagName
+  })
+    .then(tagData => res.json(tagData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    }
+  })
+    // update a tag's name by its `id` value
+    .then(tagData => {
+      if (!tagData[0]) {
+        res.status(404).json({ message: "No tag given the id" });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  // use destroy to delete
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    }
+  })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: "No tag given the id" });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
