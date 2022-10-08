2a2485e5b757fe8a2574e1f859beb860bb3c9447
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-10-07 22:57:54 -0700

    Finished up the models for category, product, and product tag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -4,17 +4,35 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
+ProductTag.init({ 
+  id: {
     // define columns
+    type: DataTypes.STRING,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
   },
-  {
+  product_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'product',
+      key: id
+    }
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'tag',
+      key: 'id'
+    }
+  }
+ }, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'product_tag',
-  }
-);
+})
+
 
 module.exports = ProductTag;
